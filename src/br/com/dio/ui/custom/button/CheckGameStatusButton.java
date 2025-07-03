456a7246fbf8879de75caade2b1aa26371ff1ed2
Diogo Mota <diogo_mota1990@outlook.com>
2025-07-03 16:50:28 -0300

    Criação UICustom com os packages button(botões), panel(telas) e frame

---
+++ src/br/com/dio/ui/custom/button/CheckGameStatusButton.java
@@ -0,0 +1,12 @@
+package br.com.dio.ui.custom.button;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class CheckGameStatusButton extends JButton {
+
+    public CheckGameStatusButton(ActionListener actionListener){
+        this.setText("Verifiar jogo");
+        this.addActionListener(actionListener);
+    }
+}
